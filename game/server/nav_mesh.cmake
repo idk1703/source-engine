2dad9f2929fd53456508e342f520c00e9372116d
idk1703 <artem.veschitsky@gmail.com>
2024-01-30 16:22:34 +0200

    Init CMake

---
+++ game/server/nav_mesh.cmake
@@ -0,0 +1,17 @@
+list(APPEND GAME_DEFINES
+	USE_NAV_MESH
+)
+list(APPEND SOURCES
+	nav_area.cpp
+	nav_colors.cpp
+	nav_edit.cpp
+	nav_entities.cpp
+	nav_file.cpp
+	nav_generate.cpp
+	nav_ladder.cpp
+	nav_merge.cpp
+	nav_mesh.cpp
+	nav_mesh_factory.cpp
+	nav_node.cpp
+	nav_simplify.cpp
+)
